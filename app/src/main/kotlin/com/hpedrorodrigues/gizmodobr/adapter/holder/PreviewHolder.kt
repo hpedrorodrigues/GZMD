59861f74df02b4c02fc7c5107cfe77e21b10ebaa
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2016-06-25 15:10:01 -0300

    Fixing build

--- app/src/main/kotlin/com/hpedrorodrigues/gizmodobr/adapter/holder/PreviewHolder.kt
+++ app/src/main/kotlin/com/hpedrorodrigues/gizmodobr/adapter/holder/PreviewHolder.kt
@@ -9,7 +9,7 @@ class PreviewHolder(view: View) : BaseHolder(view) {
 
     lateinit var title: TextView
     lateinit var authorName: TextView
-    lateinit var postAt: TextView
+    lateinit var postedAt: TextView
     lateinit var sharesCount: TextView
     lateinit var commentsCount: TextView
     lateinit var image: LabelImageView
@@ -17,7 +17,7 @@ class PreviewHolder(view: View) : BaseHolder(view) {
     override fun createView(view: View) {
         title = view.findViewById(R.id.title) as TextView
         authorName = view.findViewById(R.id.author_name) as TextView
-        postAt = view.findViewById(R.id.post_at) as TextView
+        postedAt = view.findViewById(R.id.post_at) as TextView
         sharesCount = view.findViewById(R.id.shares_count) as TextView
         commentsCount = view.findViewById(R.id.comments_count) as TextView
         image = view.findViewById(R.id.preview_image) as LabelImageView
